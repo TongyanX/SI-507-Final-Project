b06168178e7b6bd201d5bc557af28d0cf934d176
Tongyan <tongyan@umich.edu>
2018-04-03 19:54:01 -0400

    Modified Secret File

---
+++ project/secrets/secret_file.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Secret Code"""
+
+# google_places_key = 'AIzaSyBjXjxC7JwlihCOrzeYbzb1dsDMJHxakCY'
+# google_places_key = 'AIzaSyDH8mtwEc67tubMmzmZ5LkyQe_OAx5HImw'
+google_places_key = 'AIzaSyBCtbUaHmv_2n__nCXoTaR3NjjLZPZg4FA'
+
+mapbox_access_token = 'pk.eyJ1IjoicGhvZWxpdXMiLCJhIjoiY2pkeHBhdmcxMDIxdTJ3czlhdHJpZnZkNCJ9.jLNkdcmE-64UD3GfnHB61g'
